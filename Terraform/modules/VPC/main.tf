3262f9fdd6bf122963905ec8cae4b61bd6fcd165
Kishoreus <137713866+Kishoreus@users.noreply.github.com>
2026-01-13 21:00:01 +0530

    Create main.tf

---
+++ Terraform/modules/VPC/main.tf
@@ -0,0 +1,16 @@
+resource "aws_vpc" "s-vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "${var.env}-vpc"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.s-vpc.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.env}-public-subnet"
+  }
+}
