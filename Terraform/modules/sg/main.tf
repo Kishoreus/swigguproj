3e50993d7bd1608da96d69c7ca948ca85e2507ae
Kishoreus <137713866+Kishoreus@users.noreply.github.com>
2026-01-13 21:07:39 +0530

    Create main.tf

---
+++ Terraform/modules/sg/main.tf
@@ -0,0 +1,39 @@
+resource "aws_security_group" "this" {
+  name   = "${var.env}-sg"
+  vpc_id = var.vpc_id
+
+  ingress {
+    description = "SSH access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "for jenkins"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Port 9000"
+    from_port   = 9000
+    to_port     = 9000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.env}-sg"
+  }
+}
