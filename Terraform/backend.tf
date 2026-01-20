5e4e439c06a73715e0d50c87c45dfa0021c55d87
Kishoreus <137713866+Kishoreus@users.noreply.github.com>
2026-01-20 21:14:19 +0530

    Update backend.tf

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket  = "backend-state-bucket"
     key     = "terraform.tfstate"
-    region  = "ap-south-1"
+    region  = "eu-north-1"
     encrypt = true
   }
 }
