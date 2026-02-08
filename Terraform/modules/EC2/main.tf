825b4c5e6c56ba1d874f8c2527c344cca35a60f6
Kishoreus <137713866+Kishoreus@users.noreply.github.com>
2026-02-08 19:22:04 +0530

    Update main.tf

--- Terraform/modules/EC2/main.tf
+++ Terraform/modules/EC2/main.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "EC2" {
-  count                  = var.i_count
-  ami                    = var.ami_id
+  count                   = var.i_count
+  ami                     = var.ami_id
   instance_type           = var.instance_type
   subnet_id               = var.subnet_id
-  iam_instance_profile   = var.instance_profile
+  iam_instance_profile    = var.instance_profile
   vpc_security_group_ids  = [var.security_group]
 
   tags = {
